b09930c5f835709d4568b7fef78b2b11623ab5a2
Terry Tai <poshboytl@gmail.com>
2013-02-03 03:13:04 +0800

    Remove the some unneccessary spaces.

--- lib/bock/resource.rb
+++ lib/bock/resource.rb
@@ -1,7 +1,7 @@
 module Bock
 
   module Resource
-  
+
   end
 
 end
--- spec/helpers/resource_mock.rb
+++ spec/helpers/resource_mock.rb
@@ -2,9 +2,9 @@ require 'ostruct'
 require 'bock/resource/expression'
 
 class ResourceMock
-  
+
   include Bock::Resource::Expression
-  
+
   ERB_1 = <<-EOF
     <h1><%= @options.title %></h1>
     <h2><%= @options.auther %></h2>
@@ -13,17 +13,17 @@ class ResourceMock
       <%= @content %>
     </div>
   EOF
-  
+
   def initialize options={}
     @default_options = {title: 'Hello', auther: 'Zack Yang'}
     @default_options.merge!(options)
     @options = OpenStruct.new @default_options
   end
-  
+
   def to_html
     "<h5>Hello World</h5>"
   end
-  
+
   def self.build num
     resources = []
     num.times do |i|
@@ -31,5 +31,5 @@ class ResourceMock
     end
     resources
   end
-  
+
 end
